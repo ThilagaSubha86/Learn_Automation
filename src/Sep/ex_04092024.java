c45dd215e714b850b23ac52846f50f60808d4a51
ThilagaSubha86 <qatopicexplore@gmail.com>
2024-09-10 16:41:20 +0530

    Hello World

---
+++ src/Sep/ex_04092024.java
@@ -0,0 +1,9 @@
+package Sep;
+
+public class ex_04092024 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("2+2");
+        System.out.println("2-2");
+    }
+}
