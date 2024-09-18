dff57b892490b7361c053a02af7467eb02b4f79a
ThilagaSubha86 <qatopicexplore@gmail.com>
2024-09-18 15:25:45 +0530

    Printf Table

--- src/Sep/Task1_Sep_13.java
+++ src/Sep/Task1_Sep_13.java
@@ -1,6 +1,6 @@
 package Sep;
 
-public class test {
+public class Task1_Sep_13 {
     public static void main(String[] args) {
         int num = 10;
         System.out.printf("*%d * %d = %d%n" , num,1,num*1);
